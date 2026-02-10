f37652c213fc8731a467974f98913731e8b634c2
Saktra007 <gamingst410@gmail.com.com>
2026-02-10 21:19:20 +0700

    add data

--- assets/js/products.js
+++ assets/js/products.js
@@ -4,20 +4,159 @@ export const products = [
     name: "Strawberry Donut",
     price: 1.99,
     image:
-      "https://tse4.mm.bing.net/th?id=OIP.6MqYsaZt8JgVVXw3uysxUQHaHa&pid=Api&P=0&h=220",
+      "https://images.unsplash.com/photo-1551024601-bec78aea704b?w=300&h=200&fit=crop",
+    category: "Donuts",
   },
   {
     id: 2,
     name: "Chocolate Cake",
     price: 3.49,
     image:
-      "https://tse2.mm.bing.net/th?id=OIP.UsTdcdwI9KQNitBaLD8rSgHaFx&pid=Api&P=0&h=220",
+      "https://images.unsplash.com/photo-1578985545062-69928b1d9587?w=300&h=200&fit=crop",
+    category: "Cakes",
   },
   {
     id: 3,
     name: "Macaron Pack",
     price: 4.99,
     image:
-      "https://tse4.mm.bing.net/th?id=OIP.uJAHgRy1g8aEgm_-9TXTnQHaE-&pid=Api&P=0&h=220",
+      "https://tse4.mm.bing.net/th/id/OIP.CBZ9QEL2fihHPR9E0RS1ygHaHa?pid=Api&P=0&h=220p",
+    category: "Pastries",
+  },
+  {
+    id: 4,
+    name: "Blueberry Muffin",
+    price: 2.5,
+    image:
+      "https://images.unsplash.com/photo-1558401391-7899b4bd5bbf?w=300&h=200&fit=crop",
+    category: "Muffins",
+  },
+  {
+    id: 5,
+    name: "Red Velvet Cupcake",
+    price: 2.75,
+    image:
+      "https://images.unsplash.com/photo-1614707267537-b85aaf00c4b7?w=300&h=200&fit=crop",
+    category: "Cupcakes",
+  },
+  {
+    id: 6,
+    name: "Glazed Cinnamon Roll",
+    price: 3.25,
+    image:
+      "https://images.unsplash.com/photo-1509365465985-25d11c17e812?w=300&h=200&fit=crop",
+    category: "Pastries",
+  },
+  {
+    id: 7,
+    name: "Dark Chocolate Brownie",
+    price: 2.25,
+    image:
+      "https://images.unsplash.com/photo-1606312619070-d48b4c652a52?w=300&h=200&fit=crop",
+    category: "Cakes",
+  },
+  {
+    id: 8,
+    name: "Caramel Cheesecake",
+    price: 4.5,
+    image:
+      "https://images.unsplash.com/photo-1533134242443-d4fd215305ad?w=300&h=200&fit=crop",
+    category: "Cakes",
+  },
+  {
+    id: 9,
+    name: "Matcha Green Tea Donut",
+    price: 2.1,
+    image:
+      "https://images.unsplash.com/photo-1612240498936-65f5101365d2?w=300&h=200&fit=crop",
+    category: "Donuts",
+  },
+  {
+    id: 10,
+    name: "Vanilla Bean Eclair",
+    price: 3.75,
+    image:
+      "https://tse4.mm.bing.net/th/id/OIP.haEQSjBSlg1KzZ4lDGXXjgHaE1?pid=Api&P=0&h=220",
+    category: "Pastries",
+  },
+  {
+    id: 11,
+    name: "Lemon Tart",
+    price: 4.0,
+    image:
+      "https://images.unsplash.com/photo-1519915028121-7d3463d20b13?w=300&h=200&fit=crop",
+    category: "Pastries",
+  },
+  {
+    id: 12,
+    name: "Classic Croissant",
+    price: 2.5,
+    image:
+      "https://images.unsplash.com/photo-1555507036-ab1f4038808a?w=300&h=200&fit=crop",
+    category: "Pastries",
+  },
+  {
+    id: 13,
+    name: "Tiramisu Cup",
+    price: 4.25,
+    image:
+      "https://images.unsplash.com/photo-1571877227200-a0d98ea607e9?w=300&h=200&fit=crop",
+    category: "Cakes",
+  },
+  {
+    id: 14,
+    name: "Double Choc Cookie",
+    price: 1.5,
+    image:
+      "https://images.unsplash.com/photo-1499636136210-6f4ee915583e?w=300&h=200&fit=crop",
+    category: "Cookies",
+  },
+  {
+    id: 15,
+    name: "Oatmeal Raisin Cookie",
+    price: 1.5,
+    image:
+      "https://tse3.mm.bing.net/th/id/OIP.qjUkhhCtfhEgsRp7O0om8gHaE8?pid=Api&P=0&h=220",
+    category: "Cookies",
+  },
+  {
+    id: 16,
+    name: "Rainbow Sprinkles Donut",
+    price: 1.85,
+    image:
+      "https://images.unsplash.com/photo-1527515637462-cff94eecc1ac?w=300&h=200&fit=crop",
+    category: "Donuts",
+  },
+  {
+    id: 17,
+    name: "Apple Turnover",
+    price: 2.95,
+    image:
+      "https://tse1.mm.bing.net/th/id/OIP.kEjbO6iwJiPJV90N3sXT6wHaHa?pid=Api&P=0&h=220",
+    category: "Pastries",
+  },
+  {
+    id: 18,
+    name: "Pistachio Cake Slice",
+    price: 3.8,
+    image:
+      "https://images.unsplash.com/photo-1542826438-bd32f43d626f?w=300&h=200&fit=crop",
+    category: "Cakes",
+  },
+  {
+    id: 19,
+    name: "Banana Nut Bread",
+    price: 2.6,
+    image:
+      "https://images.unsplash.com/photo-1530610476181-d83430b64dcd?w=300&h=200&fit=crop",
+    category: "Pastries",
+  },
+  {
+    id: 20,
+    name: "Salted Caramel Donut",
+    price: 2.2,
+    image:
+      "https://images.unsplash.com/photo-1533910534207-90f31029a78e?w=300&h=200&fit=crop",
+    category: "Donuts",
   },
 ];
